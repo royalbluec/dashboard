c93468db18215eafb2e70bf00bf9984a17a9b64f
royalbluee <tkyeom96@gmail.com>
2022-06-15 21:07:05 +0900

    feat: Add DailyActiveUserChart component

--- src/components/DailyActiveUserChart.vue
+++ src/components/DailyActiveUserChart.vue
@@ -1,10 +1,27 @@
 <template lang="">
   <div>DailyActiveUserChart</div>
+  <div>{{ eventList.length }}</div>
+  <div>{{ eventList }}</div>
 </template>
 
 <script>
+import { getEventOneAPI } from '../api/index';
+
 export default {
   name: 'DailyActiveUserChart',
+  data() {
+    return {
+      eventList: [],
+    };
+  },
+  mounted() {
+    this.setList();
+  },
+  methods: {
+    async setList() {
+      this.eventList = await getEventOneAPI();
+    },
+  },
 };
 </script>
 
